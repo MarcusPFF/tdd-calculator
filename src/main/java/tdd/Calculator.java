0b7d32e21eb2afc8ac6e680f3884be73f2b0e250
MarcusPFF <marcus.pff03@gmail.com>
2025-02-03 10:36:58 +0100

    small details

--- src/main/java/tdd/Calculator.java
+++ src/main/java/tdd/Calculator.java
@@ -27,15 +27,16 @@ public class Calculator {
         }
         return sum;
     }
-        public int subtract(int a, int b) {
+
+    public int subtract(int a, int b) {
             return a - b;
         }
 
-        public int multiply(int a, int b) {
+    public int multiply(int a, int b) {
             return a * b;
         }
 
-        public int divide(int a, int b) {
+    public int divide(int a, int b) {
             if (b == 0) {
                 throw new ArithmeticException("Error, divide by zero");
             }
